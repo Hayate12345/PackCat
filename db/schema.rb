4785b8a99190b96111e15e2ee2fd03184b7def32
Hayate_T <hayatesato1@gmail.com>
2023-02-14 22:30:26 +0900

    postsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_14_095727) do
+ActiveRecord::Schema.define(version: 2023_02_14_131808) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "content"
+    t.string "image"
+    t.string "amount"
+    t.string "place"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -41,4 +52,5 @@ ActiveRecord::Schema.define(version: 2023_02_14_095727) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
