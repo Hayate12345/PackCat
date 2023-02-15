50b85a887d76e9aab1e32ebfe080391d362ca2db
Hayate_T <hayatesato1@gmail.com>
2023-02-15 20:09:21 +0900

    プロフィールのレイアウト修正

--- app/controllers/profiles/profiles_controller.rb
+++ app/controllers/profiles/profiles_controller.rb
@@ -2,5 +2,6 @@ class Profiles::ProfilesController < ApplicationController
   # ! ログイン中のユーザ情報を表示するメソッド
   def show
     @user = User.find(params[:user_id])
+    @posts = Post.where(params[:user_id]).order(created_at: "DESC")
   end
 end
