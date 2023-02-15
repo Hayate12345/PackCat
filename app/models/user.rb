55356b6bd925aaf1b736c3b4e55f25c1ae7f4bef
Hayate_T <hayatesato1@gmail.com>
2023-02-16 00:07:28 +0900

    eventsテーブルの作成、eventsテーブルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   # ! アソシエーションの定義 (has_manyは複数形、belongs_toは単数形で記述)
   # * postsテーブルとのアソシエーション (usersテーブルからみて、postsテーブルは多の関係)
   has_many :posts
+  # * eventsテーブルとのアソシエーション (usersテーブルからみて、eventsテーブルは多の関係)
+  has_many :posts
   # * likesテーブルとのアソシエーション （usersテーブルからみて、likesテーブルは多の関係)
   has_many :likes
   # * commentsテーブルとのアソシエーション （postsテーブルから見て、commentsテーブルは多の関係）
